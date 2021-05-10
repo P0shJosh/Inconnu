5c06d314f874010fed16462b8443d0b4a1d9e792
P0shJosh <jphodge.96@gmail.com>
2021-05-10 18:29:40 +0100

    FIX: Change setup from Realpython tutorial to James S Youtube tutorial

--- bot.py
+++ bot.py
@@ -1,18 +1,21 @@
-# bot.py
 import os
 
 import discord
-from dotenv import load_dotenv
+from discord.ext import commands
 if os.path.exists("env.py"):
     import env
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+client = commands.Bot(command_prefix = '!')
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print('The bot is now ready for use!')
+    
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hello, I am Inconnu")
+    
 
 client.run(TOKEN)
