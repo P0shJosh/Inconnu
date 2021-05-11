b648f42ca64b698caeafce815d39a61b62b68faa
P0shJosh <jphodge.96@gmail.com>
2021-05-11 15:45:26 +0100

    FEAT: Roll quantity of d10s with single command
    If the discord user begins a message with !roll followed by an integer (ie !roll 5), the bot will roll x amount of d10s and then display the results to users in a code block

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 
 import discord
+import random
 from discord.ext import commands
 if os.path.exists("env.py"):
     import env
@@ -16,6 +17,17 @@ async def on_ready():
 @client.command()
 async def hello(ctx):
     await ctx.send("Hello, I am Inconnu")
-    
+
+
+@client.command()
+async def roll(ctx, quantity):
+  try:
+    dice_pool = []
+    for number in range(int(quantity)):
+       dice_pool.append(random.randint(1, 10))
+    results = ", ".join(str(number) for number in dice_pool)
+    await ctx.send("```" + results + "```")
+  except Exception as x:
+    await ctx.send("Idiot. !roll 'integer' with integer being your dice pool.")
 
 client.run(TOKEN)
